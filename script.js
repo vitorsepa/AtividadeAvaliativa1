995d92dea89c398efc6fbddbfd05c4e0a61a9842
VITOR COSTA CARDOSO DA LUZ <vitor_c_luz@estudante.sesisenai.org.br>
2025-03-24 09:47:52 -0300

    colocando a pontuacao

--- script.js
+++ script.js
@@ -36,7 +36,8 @@ class Entidade {
 
 class Cobra extends Entidade {
    constructor(x, y, largura, altura) {
-       super(x, y, largura, altura)
+       super(x, y, largura, altura);
+       this.pontuacao = 0
    }
    atualizar() {
     if (teclasPressionadas.KeyW) {
@@ -66,26 +67,35 @@ class Cobra extends Entidade {
            this.x + this.largura > comida.x &&
            this.y < comida.y + comida.altura &&
            this.y + this.altura > comida.y
-       ){ 
+       ){   
            this.#houveColisao(comida)
        }
+
    }
    #houveColisao(comida){
        comida.x = Math.random()*canvas.width-10
        comida.y = Math.random()*canvas.height-10
+       this.pontuacao++;
    }
-}
+
+   desenharPontuacao() {
+    ctx.fillStyle = 'black';
+    ctx.font = '20px Arial';
+    ctx.fillText(`Pontuação: ${this.pontuacao}`, 10, 20);
+    }
+   }
+
 class Comida extends Entidade {
     constructor() {
-        const x = Math.max(Math.random() * (canvas.width - 20), 0); // Evita valores negativos
-        const y = Math.max(Math.random() * (canvas.height - 20), 0); // Evita valores negativos
-        super(x, y, 20, 20); // Tamanho da comida
+        const x = Math.max(Math.random() * (canvas.width - 20), 0);
+        const y = Math.max(Math.random() * (canvas.height - 20), 0);
+        super(x, y, 20, 20);
         this.imagem = new Image();
         this.imagem.src = 'https://www.imagenspng.com.br/wp-content/uploads/2015/04/branca-de-neve-cute-maca-02.png'; // URL da imagem
     }
  
     desenhar() {
-        ctx.drawImage(this.imagem, this.x, this.y, this.largura, this.altura); // Desenha a imagem no canvas
+        ctx.drawImage(this.imagem, this.x, this.y, this.largura, this.altura);
     }
  }
 
@@ -99,6 +109,7 @@ function loop() {
     cobra.atualizar();
     comida.desenhar();
     cobra.verificarColisao(comida);
+    cobra.desenharPontuacao();
     requestAnimationFrame(loop);
  }
  
