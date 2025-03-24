6de3d7b43d33d18abf611ababa15b7d287a0d45c
VITOR COSTA CARDOSO DA LUZ <vitor_c_luz@estudante.sesisenai.org.br>
2025-03-24 09:24:26 -0300

    maquiando o site

--- script.js
+++ script.js
@@ -39,16 +39,27 @@ class Cobra extends Entidade {
        super(x, y, largura, altura)
    }
    atualizar() {
-       if (teclasPressionadas.KeyW) {
-           this.y -= 7
-       } else if (teclasPressionadas.KeyS) {
-           this.y += 7
-       } else if (teclasPressionadas.KeyA) {
-           this.x -= 7
-       } else if (teclasPressionadas.KeyD) {
-           this.x += 7
-       }
-   }
+    if (teclasPressionadas.KeyW) {
+        this.y -= 7;
+    } else if (teclasPressionadas.KeyS) {
+        this.y += 7;
+    } else if (teclasPressionadas.KeyA) {
+        this.x -= 7;
+    } else if (teclasPressionadas.KeyD) {
+        this.x += 7;
+    }
+ 
+    if (this.x < 0 || this.x + this.largura > canvas.width || 
+        this.y < 0 || this.y + this.altura > canvas.height) {
+        this.#fimDeJogo();
+    }
+ }
+ 
+ #fimDeJogo() {
+    alert("Game over! \nA cobra tocou na borda!");
+    window.location.reload();
+ }
+ 
    verificarColisao(comida){
        if(
            this.x < comida.x + comida.largura &&
@@ -65,22 +76,30 @@ class Cobra extends Entidade {
    }
 }
 class Comida extends Entidade {
-   constructor() {
-       super(Math.random()*canvas.width-10,    Math.random()*canvas.height-10, 20, 20)
-   }
-}
-
+    constructor() {
+        const x = Math.max(Math.random() * (canvas.width - 20), 0); // Evita valores negativos
+        const y = Math.max(Math.random() * (canvas.height - 20), 0); // Evita valores negativos
+        super(x, y, 20, 20); // Tamanho da comida
+        this.imagem = new Image();
+        this.imagem.src = 'https://www.imagenspng.com.br/wp-content/uploads/2015/04/branca-de-neve-cute-maca-02.png'; // URL da imagem
+    }
+ 
+    desenhar() {
+        ctx.drawImage(this.imagem, this.x, this.y, this.largura, this.altura); // Desenha a imagem no canvas
+    }
+ }
 
 const cobra = new Cobra(100, 200, 20, 20)
 const comida = new Comida()
 
 
 function loop() {
-   ctx.clearRect(0, 0, canvas.width, canvas.height)
-   cobra.desenhar()
-   cobra.atualizar()
-   comida.desenhar()
-   cobra.verificarColisao(comida)
-   requestAnimationFrame(loop)
-}
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    cobra.desenhar();
+    cobra.atualizar();
+    comida.desenhar();
+    cobra.verificarColisao(comida);
+    requestAnimationFrame(loop);
+ }
+ 
 loop()
